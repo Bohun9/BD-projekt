2dcfa7ede49d8474ed3ad1edf4f8be5e74d67a1f
Marcin Martowicz <marcinmartowicz9@gmail.com>
2023-06-06 13:05:08 +0200

    basic conceptual model

---
+++ protest/__init__.py
@@ -0,0 +1,23 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(SECRET_KEY="dev", DATABASE="baza_main")
+    app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route("/hello")
+    def hello():
+        return "Hello, World!"
+
+    return app
--- protest/schema.sql
+++ protest/schema.sql
@@ -1,8 +1,21 @@
+-- to clear db find topological order of tables in dependency graph
+DROP TABLE IF EXISTS Participation;
+DROP TABLE IF EXISTS Report;
+DROP TABLE IF EXISTS Protection;
+DROP TABLE IF EXISTS Worldview;
+DROP TABLE IF EXISTS Protest;
+DROP TABLE IF EXISTS Guard;
+DROP TABLE IF EXISTS GovernmentAction;
+DROP TABLE IF EXISTS OrganizationMember;
+
+
 CREATE TABLE OrganizationMember(
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     last_name VARCHAR(255),
     age INTEGER CHECK (age >= 18),
+    login VARCHAR(10),
+    hashed_password VARCHAR(255),
     organizer_privilege BOOLEAN
 );
 
@@ -16,34 +29,40 @@ CREATE TABLE Protest(
     id SERIAL PRIMARY KEY,
     organizer_id INTEGER REFERENCES OrganizationMember(id),
     action_id INTEGER REFERENCES GovernmentAction(id),
-    start_time DATETIME,
+    start_time TIMESTAMP,
     town VARCHAR(255),
-    geographical_coordinate VARCHAR(255),
+    coordinates POINT,
     boombox_number INTEGER NOT NULL CHECK (boombox_number > 0)
 );
 
 CREATE TABLE Participation(
     member_id INTEGER REFERENCES OrganizationMember(id),
-    protest_id INTEGER REFERENCES Protest(id),
+    protest_id INTEGER REFERENCES Protest(id)
 );
 
 CREATE TABLE Report(
     member_id INTEGER REFERENCES OrganizationMember(id),
     protest_id INTEGER REFERENCES Protest(id),
     rating INTEGER NOT NULL CHECK (1 <= rating AND rating <= 10),
-    description VARCHAR(255)
+    description VARCHAR(1024)
 );
 
 CREATE TABLE Guard(
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     last_name VARCHAR(255),
+    added_by INTEGER REFERENCES OrganizationMember(id),
     weight INTEGER NOT NULL CHECK (weight >= 80),
-    running_speed INTEGER NOT NULL CHECK (running_speed >= 20),
+    running_speed INTEGER NOT NULL CHECK (running_speed >= 20)
 );
 
 CREATE TABLE Protection(
     guard_id INTEGER REFERENCES Guard(id),
-    protest_id INTEGER REFERENCES Protest(id),
+    protest_id INTEGER REFERENCES Protest(id)
+);
+
+CREATE TABLE Worldview(
+    guard_id INTEGER REFERENCES Guard(id),
+    action_id INTEGER REFERENCES Protest(id)
 );
 
